9a4e39c1b29db25e9fe2e7d46404cee271a3c914
arthurtuio <arthur.henrique.2709@gmail.com>
2020-09-07 16:45:44 -0300

    fix csv call

--- teste_streamlit_ec2.py
+++ teste_streamlit_ec2.py
@@ -549,7 +549,7 @@ def create_df_and_texts():
 
     st.sidebar.header('User Input Features')
 
-    enriched_csv = "/home/arthur/PycharmProjects/TCC/CSVs/enriched_csvs/enriched_result_05_set.csv"
+    enriched_csv = "enriched_result_05_set.csv"
 
     return pd.read_csv(enriched_csv), analysis_style
 
